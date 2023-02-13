985171489200a731368f85b881733545ce6d9753
crystalhwaang <chwang610@gmail.com>
2023-02-13 00:42:59 -0700

    Add project files.

---
+++ Mission06_clhwang/Models/MovieApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_clhwang.Models
+{
+    public class MovieApplicationContext : DbContext
+    {
+        public MovieApplicationContext (DbContextOptions<MovieApplicationContext> options) : base (options)
+        {
+
+        }
+        public Microsoft.EntityFrameworkCore.DbSet<ApplicationResponse> responses { get; set; }
+    }
+}
